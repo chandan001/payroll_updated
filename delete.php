54df177865a22b64d47b730913ee64b304b53214
Chandan Kumar <adiwalchandan@gmail.com>
2020-04-17 11:41:03 -0500

    add/delete employee bug fixed

--- delete.php
+++ delete.php
@@ -1,8 +1,12 @@
 <?php 
-	require('db.php');
+//	require('db.php');
 	
 	$id=$_GET['emp_id'];
+	 $conn11 = mysqli_connect('localhost', 'adiwal', 'adiwal@1.1', 'payroll');
+
 	$query = "DELETE FROM employee WHERE emp_id=$id"; 
-	$result = mysql_query($query) or die ( mysql_error());
+mysqli_query($conn11,$query) or die("Error in Query" . mysqli_error($conn11));	
+	
+	
 	header("Location: home_employee.php"); 
  ?>
